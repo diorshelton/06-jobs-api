05a32e8b72b765cd2a8fa936f015f73b65d5df68
Dior.Shelton <dior.shelton@gmail.com>
2024-09-02 15:19:22 -0400

    add frontend code so that jobs are visible now

--- public/addEdit.js
+++ public/addEdit.js
@@ -8,25 +8,62 @@ let visible = null;
 let addingPost = null;
 
 export const handleAddEdit = () => {
-  addEditDiv = document.getElementById("edit-post");
-  title = document.getElementById("title");
-  postContent = document.getElementById("post-content");
-  visible = document.getElementById("visible");
-  addingPost = document.getElementById("adding-post");
-  const editCancel = document.getElementById("edit-cancel");
+	addEditDiv = document.getElementById("edit-post");
+	title = document.getElementById("title");
+	postContent = document.getElementById("post-content");
+	visible = document.getElementById("visible");
+	addingPost = document.getElementById("adding-post");
+	const editCancel = document.getElementById("edit-cancel");
 
-  addEditDiv.addEventListener("click", (e) => {
-    if (inputEnabled && e.target.nodeName === "BUTTON") {
-      if (e.target === addingPost) {
-        showPosts();
-      } else if (e.target === editCancel) {
-        showPosts();
-      }
-    }
-  });
+	addEditDiv.addEventListener("click", async (e) => {
+		if (inputEnabled && e.target.nodeName === "BUTTON") {
+			if (e.target === addingPost) {
+				enableInput(false);
+
+				let method = "POST";
+				let url = "/api/v1/posts";
+				try {
+					const response = await fetch(url, {
+						method: method,
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${token}`,
+						},
+						body: JSON.stringify({
+							title: title.value,
+							message: postContent.value,
+							visible: visible.value,
+						}),
+					});
+
+					const data = await response.json();
+					if (response.status === 201) {
+						// 201 indicates a successful create
+						message.textContent = "The job entry was created.";
+
+						title.value = "";
+						postContent.value = "";
+						visible.value = true;
+
+						showPosts();
+					} else {
+						message.textContent = data.msg;
+					}
+				} catch (err) {
+					console.log(err);
+					message.textContent = "A communication error occurred.";
+				}
+
+				enableInput(true);
+			}
+		} else if (e.target === editCancel) {
+			message.textContent = "";
+			showPosts();
+		}
+	});
 };
 
 export const showAddEdit = (post) => {
-  message.textContent = "";
-  setDiv(addEditDiv);
+	message.textContent = "";
+	setDiv(addEditDiv);
 };
